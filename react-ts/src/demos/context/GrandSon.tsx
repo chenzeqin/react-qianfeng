a7125d8ae14e966f9ed927c34a52d056dc1b9162
chenzeqin <sailor_chenzeqin@163.com>
2022-08-03 21:06:14 +0800

    react children props可以传入一个函数

--- react-ts/src/demos/context/GrandSon.tsx
+++ react-ts/src/demos/context/GrandSon.tsx
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, ReactNode } from 'react';
 import { myContext } from './myContext';
 
-export default class GrandSon extends Component {
+interface GrandSonProps {
+  children: () => ReactNode;
+}
+export default class GrandSon extends Component<GrandSonProps> {
   render() {
+    console.log(this.props);
     return (
       <div>
         <h5>GrandSon接收index数据</h5>
+        <div>
+          {this.props.children()}
+        </div>
         <myContext.Consumer>
           {(value) => {
             return <div>{value.info}</div>;
--- react-ts/src/demos/context/Son.tsx
+++ react-ts/src/demos/context/Son.tsx
@@ -1,13 +1,18 @@
-import React, { Component } from 'react'
-import GrandSon from './GrandSon'
+import React, { Component } from 'react';
+import GrandSon from './GrandSon';
 
 export default class Son extends Component {
   render() {
     return (
       <div>
         <h5>Son</h5>
-        <GrandSon></GrandSon>
+        <GrandSon>
+          {() => {
+            console.log('GrandSon');
+            return <div>chilren可以传入函数</div>;
+          }}
+        </GrandSon>
       </div>
-    )
+    );
   }
 }
