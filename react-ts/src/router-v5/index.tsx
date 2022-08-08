faf8d93be4fd4e6b40a5ab8a7c9f0f2fc2bf8848
chenzeqin <sailor_chenzeqin@163.com>
2022-08-09 00:18:10 +0800

    router@5： 404 not-found

--- react-ts/src/router-v5/index.tsx
+++ react-ts/src/router-v5/index.tsx
@@ -3,6 +3,7 @@ import { HashRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Films from '../router-pages/Films';
 import Cinemas from '../router-pages/Cinemas';
 import Center from '../router-pages/Center';
+import NotFound from '../router-pages/NotFound';
 
 function HomeRouter() {
   return (
@@ -12,7 +13,9 @@ function HomeRouter() {
         <Route path="/cinemas" component={Cinemas}></Route>
         <Route path="/center" component={Center}></Route>
         {/* 路由重定向 */}
-        <Redirect to="/films" from="/"></Redirect>
+        {/* <Redirect to="/films" from="/"></Redirect> */}
+        {/* 404 路由兜底页面 */}
+        <Route component={NotFound}></Route>
       </Switch>
     </HashRouter>
   );
