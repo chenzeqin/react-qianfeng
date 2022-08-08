51b7da83643700a9373a8fd4a5fa9fcc848c3949
chenzeqin <sailor_chenzeqin@163.com>
2022-08-09 00:13:56 +0800

    router@5: 重定向

--- react-ts/src/router-v5/index.tsx
+++ react-ts/src/router-v5/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Films from '../router-pages/Films';
 import Cinemas from '../router-pages/Cinemas';
 import Center from '../router-pages/Center';
@@ -7,9 +7,13 @@ import Center from '../router-pages/Center';
 function HomeRouter() {
   return (
     <HashRouter>
-      <Route path="/films" component={Films}></Route>
-      <Route path="/cinemas" component={Cinemas}></Route>
-      <Route path="/center" component={Center}></Route>
+      <Switch>
+        <Route path="/films" component={Films}></Route>
+        <Route path="/cinemas" component={Cinemas}></Route>
+        <Route path="/center" component={Center}></Route>
+        {/* 路由重定向 */}
+        <Redirect to="/films" from="/"></Redirect>
+      </Switch>
     </HashRouter>
   );
 }
