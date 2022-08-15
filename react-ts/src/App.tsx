0a40f02b2ef679968ba4dd8dd0dc1486781f04de
chenzeqin <sailor_chenzeqin@163.com>
2022-08-16 02:09:56 +0800

    antd 常用组件练习

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -3,7 +3,7 @@ import Demos from './demos';
 import HooksDemo from './demos/hooks-demo';
 import Maoyan from './proxy-demo/Maoyan';
 import Maizuo from './router-pages';
-
+import AntdDemo from './antd-demo';
 
 function App() {
   return (
@@ -11,9 +11,11 @@ function App() {
       {/* <HooksDemo></HooksDemo> */}
       {/* <Demos></Demos> */}
       {/* react-router-dom v5 demo */}
-      <Maizuo></Maizuo>
+      {/* <Maizuo></Maizuo> */}
       {/* http proxy demo */}
       {/* <Maoyan></Maoyan> */}
+      {/* Antd 组件库练习 */}
+      <AntdDemo></AntdDemo>
     </div>
   );
 }
---
+++ react-ts/src/antd-demo/index.tsx
@@ -0,0 +1,267 @@
+import React from 'react';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import './index.css';
+import {
+  DatePicker,
+  Button,
+  Col,
+  Divider,
+  Row,
+  Layout,
+  Menu,
+  Steps,
+  message,
+  Table,
+  Tree,
+} from 'antd';
+import type { DataNode, TreeProps } from 'antd/es/tree';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { useState } from 'react';
+
+const { Step } = Steps;
+
+const steps = [
+  {
+    title: 'First',
+    content: 'First-content',
+  },
+  {
+    title: 'Second',
+    content: 'Second-content',
+  },
+  {
+    title: 'Last',
+    content: 'Last-content',
+  },
+];
+// table
+const dataSource = [
+  {
+    key: '1',
+    name: '胡彦斌',
+    age: 32,
+    address: '西湖区湖底公园1号',
+  },
+  {
+    key: '2',
+    name: '胡彦祖',
+    age: 42,
+    address: '西湖区湖底公园1号',
+  },
+];
+const columns = [
+  {
+    title: '姓名',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: '年龄',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: '住址',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
+
+const treeData: DataNode[] = [
+  {
+    title: 'parent 1',
+    key: '0-0',
+    children: [
+      {
+        title: 'parent 1-0',
+        key: '0-0-0',
+        disabled: true,
+        children: [
+          {
+            title: 'leaf',
+            key: '0-0-0-0',
+            disableCheckbox: true,
+          },
+          {
+            title: 'leaf',
+            key: '0-0-0-1',
+          },
+        ],
+      },
+      {
+        title: 'parent 1-1',
+        key: '0-0-1',
+        children: [
+          {
+            title: <span style={{ color: '#1890ff' }}>sss</span>,
+            key: '0-0-1-0',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const style: React.CSSProperties = { background: '#0092ff', padding: '8px 0' };
+const { Header, Sider, Content } = Layout;
+
+export default function AntdDemo() {
+  // 菜单收起展开
+  const [collapsed, setCollapsed] = useState(false);
+  // 步骤
+  const [current, setCurrent] = useState(0);
+  const next = () => {
+    setCurrent(current + 1);
+  };
+
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+
+  // tree
+  const onSelect: TreeProps['onSelect'] = (selectedKeys, info) => {
+    console.log('selected', selectedKeys, info);
+  };
+
+  const onCheck: TreeProps['onCheck'] = (checkedKeys, info) => {
+    console.log('onCheck', checkedKeys, info);
+  };
+
+  return (
+    <div>
+      <h2>AntdDemo</h2>
+      <h3>日期选择</h3>
+      <div>
+        <DatePicker />
+      </div>
+      <h3>按钮</h3>
+      <div>
+        <Button
+          type="primary"
+          onClick={() => {
+            console.log('click ok');
+          }}
+        >
+          Primary Button
+        </Button>
+        <Button>Default Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+        <Button type="text">Text Button</Button>
+        <Button type="link">Link Button</Button>
+      </div>
+      <h3>布局-栅格系统</h3>
+      <div>
+        <Divider orientation="left">Horizontal</Divider>
+        <Row gutter={16}>
+          <Col className="gutter-row" span={6}>
+            <div style={style}>col-6</div>
+          </Col>
+          <Col className="gutter-row" span={6}>
+            <div style={style}>col-6</div>
+          </Col>
+          <Col className="gutter-row" span={6}>
+            <div style={style}>col-6</div>
+          </Col>
+          <Col className="gutter-row" span={6}>
+            <div style={style}>col-6</div>
+          </Col>
+        </Row>
+      </div>
+      <h3>页面布局</h3>
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            items={[
+              {
+                key: '1',
+                icon: <UserOutlined />,
+                label: 'nav 1',
+              },
+              {
+                key: '2',
+                icon: <VideoCameraOutlined />,
+                label: 'nav 2',
+              },
+              {
+                key: '3',
+                icon: <UploadOutlined />,
+                label: 'nav 3',
+              },
+            ]}
+          />
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            {React.createElement(
+              collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+              {
+                className: 'trigger',
+                onClick: () => setCollapsed(!collapsed),
+              }
+            )}
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+      <h3>step 步骤</h3>
+      <div>
+        <Steps current={current}>
+          {steps.map((item) => (
+            <Step key={item.title} title={item.title} />
+          ))}
+        </Steps>
+        <div className="steps-content">{steps[current].content}</div>
+        <div className="steps-action">
+          {current < steps.length - 1 && (
+            <Button type="primary" onClick={() => next()}>
+              Next
+            </Button>
+          )}
+          {current === steps.length - 1 && (
+            <Button
+              type="primary"
+              onClick={() => message.success('Processing complete!')}
+            >
+              Done
+            </Button>
+          )}
+          {current > 0 && (
+            <Button style={{ margin: '0 8px' }} onClick={() => prev()}>
+              Previous
+            </Button>
+          )}
+        </div>
+      </div>
+      <h3>table</h3>
+      <Table dataSource={dataSource} columns={columns} />;<h3>tree</h3>
+      <Tree
+        checkable
+        defaultExpandedKeys={['0-0-0', '0-0-1']}
+        defaultSelectedKeys={['0-0-0', '0-0-1']}
+        defaultCheckedKeys={['0-0-0', '0-0-1']}
+        onSelect={onSelect}
+        onCheck={onCheck}
+        treeData={treeData}
+      />
+    </div>
+  );
+}
