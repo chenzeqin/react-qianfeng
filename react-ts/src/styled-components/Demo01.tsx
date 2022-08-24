630aac9e4384188aa06670e63c40ef39a5cf03e8
chenzeqin <sailor_chenzeqin@163.com>
2022-08-25 01:15:38 +0800

    styled-components: 基本使用-属性透传

--- react-ts/src/styled-components/Demo01.tsx
+++ react-ts/src/styled-components/Demo01.tsx
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import styled from 'styled-components';
-export default class Demo01 extends Component {
+
+interface Props {
+  color?: string;
+}
+export default class Demo01 extends Component<Props> {
   render() {
     const StyleDiv = styled.div`
-      background-color: pink;
+      background-color: ${(props: Props) => props.color};
       font-weight: 600;
       line-height: 30px;
       border: 2px solid #ccc;
@@ -15,18 +19,25 @@ export default class Demo01 extends Component {
         color: #fff;
       }
     `;
+    const StyledInput = styled.input`
+      background: blue;
+      color: #fff;
+    `;
 
     return (
-      <div>
-        <StyleDiv>
-          <div>demo01</div>
-          <ul className="list">
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-          </ul>
-        </StyleDiv>
-      </div>
+      //  TODO: 需要优化性能
+      // StyleDiv 会创建一个div 每次重新渲染会重新创建，input状态也会丢失
+      <StyleDiv color="green">
+        <div>demo01</div>
+        <input type="text" />
+        <ul className="list">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+        {/* 支持属性透传 */}
+        <StyledInput placeholder="请输入"></StyledInput>
+      </StyleDiv>
     );
   }
 }
--- react-ts/src/styled-components/index.tsx
+++ react-ts/src/styled-components/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Demo01 from './Demo01';
 
 export default function StyledComponents() {
+  const [color, setColor] = useState('pink');
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const a = Math.floor(Math.random() * 17);
+      const b = Math.floor(Math.random() * 17);
+      const c = Math.floor(Math.random() * 17);
+
+      // setColor(`#${a > 9 ? a : 0 + a}${b > 9 ? b : 0 + b}${c > 9 ? c : 0 + c}`);
+    }, 20000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
   return (
     <div>
       <h2>StyledComponents</h2>
-      <Demo01></Demo01>
+      <Demo01 color={color}></Demo01>
     </div>
   );
 }
