c8a714499dc7398e5faf9f8c8eb5cf434433ee73
chenzeqin <sailor_chenzeqin@163.com>
2022-08-28 02:01:30 +0800

    generator： next传入参数

--- react-ts/src/redux-saga/generator/index.ts
+++ react-ts/src/redux-saga/generator/index.ts
@@ -1,20 +1,20 @@
 
 export function* test() {
   console.log('1')
-  yield 'a'  // 中断函数执行
-  console.log('2')
-  yield 'b'// 中断函数执行
-  console.log('3')
+  const input1: string = yield 'a'  // 中断函数执行
+  console.log('2-valueFromNext:', input1) // A
+  const input2: string = yield 'b'// 中断函数执行
+  console.log('3-valueFromNext:', input2) // B
 }
 
 
 const g = test();
 // 执行一次就会产生一个状态值
-const res1 = g.next(); // 1
+const res1 = g.next('第一个无效'); // 1
 console.log('res1', res1) // {done: false, value: "a"}
-const res2 = g.next(); // 2
+const res2 = g.next('A'); // 2
 console.log('res2', res2) // {done: false, value: "b"}
-const res3 = g.next(); // 3
+const res3 = g.next('B'); // 3
 console.log('res3', res3) // {done: true, value: undefined}
-const res4 = g.next(); // 没有执行的console.log
+const res4 = g.next("C"); // 没有执行的console.log
 console.log('res4', res4) // {done: true, value: undefined}
