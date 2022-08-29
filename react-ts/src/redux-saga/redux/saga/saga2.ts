6e6028554a9a5e47dd4f671f9700f35dee9901e6
chenzeqin <sailor_chenzeqin@163.com>
2022-08-29 22:09:56 +0800

    saga: 链式调用

--- react-ts/src/redux-saga/redux/saga/saga2.ts
+++ react-ts/src/redux-saga/redux/saga/saga2.ts
@@ -12,18 +12,31 @@ export function* watchSaga2() {
 
 function* getList() {
   // call：  函数发异步请求
-  const res: string[] = yield call(getListPromise)
+  const res1: string[] = yield call(getListPromise1)
+  const res2: string[] = yield call(getListPromise2, res1)
 
   yield put({
     type: 'change-list2',
-    payload: res
+    payload: res2
   })
 }
 
-function getListPromise() {
+function getListPromise1() {
   return new Promise<any>((resolve, reject) => {
     setTimeout(() => {
       resolve([
+        '444444',
+        '555555',
+        '666666',
+      ])
+    })
+  })
+}
+function getListPromise2(data1: string[]) {
+  return new Promise<any>((resolve, reject) => {
+    setTimeout(() => {
+      resolve([
+        ...data1,
         '444444',
         '555555',
         '666666',
