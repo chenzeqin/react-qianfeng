554abad831b71d2e4db6a0b5bde34d19994b2a84
chenzeqin <sailor_chenzeqin@163.com>
2022-09-03 12:23:54 +0800

    graphql: 传参查询

--- server/src/graphql-express.js
+++ server/src/graphql-express.js
@@ -2,25 +2,39 @@ var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 
+// getFilm(id:Int!):Film : !是必填意思
 var schema = buildSchema(`
   type Query {
     hello: String,
     name: String,
     age: Int,
     habits: [String]
-    getAcount: Acount
+    getAcount: Acount,
+    getFilmList: [Film],
+    getFilm(id:Int!):Film
   }
   type Acount {
     name: String,
     age: Int,
     locatioin: String
   }
+  type Film {
+    id: Int,
+    name:String
+  }
 `);
 
+const filmList = [
+  { id: 1, name: '泰坦尼克号' },
+  { id: 2, name: '阿甘正传' },
+];
+
 var root = {
+  // 简单类型
   hello: () => 'Hello world!',
   name: () => 'kobe',
   age: () => 24,
+  // 复杂类型
   habits: () => ['ball', 'ball', 'ball'],
   getAcount() {
     return {
@@ -28,6 +42,14 @@ var root = {
       age: 30,
       location: 'golden state',
     };
+  },
+  getFilmList() {
+    return filmList;
+  },
+  // 传参  定义是传入一个id，实参其实是obj
+  getFilm({ id }) {
+    console.log(filmList, id);
+    return filmList.find((item) => item.id === id);
   },
 };
 
