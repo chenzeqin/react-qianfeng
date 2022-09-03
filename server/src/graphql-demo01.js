46230b3afe65b423913f4b75d29c48d05618b725
chenzeqin <sailor_chenzeqin@163.com>
2022-09-03 11:03:50 +0800

    graphql: hello word

--- server/src/graphql-demo01.js
+++ server/src/graphql-demo01.js
@@ -1,11 +1,22 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var { graphql, buildSchema } = require('graphql');
 
+var app = express();
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+var rootValue = { hello: () => 'Hello world!' };
+var source = '{ hello }';
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('hello world')
-})
+  graphql({ schema, source, rootValue }).then((response) => {
+    res.send(response);
+  });
+});
 
-app.listen(3000, ()=>{
-  console.log('localhost:3000')
-})
+app.listen(3000, () => {
+  console.log('localhost:3000');
+});
