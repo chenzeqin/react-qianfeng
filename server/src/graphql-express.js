215e9c881823dd3d2b9ee5f322446e724f27a60e
chenzeqin <sailor_chenzeqin@163.com>
2022-09-03 12:07:56 +0800

    graphql: 查询复杂类型

--- server/src/graphql-express.js
+++ server/src/graphql-express.js
@@ -4,16 +4,40 @@ var { buildSchema } = require('graphql');
 
 var schema = buildSchema(`
   type Query {
-    hello: String
+    hello: String,
+    name: String,
+    age: Int,
+    habits: [String]
+    getAcount: Acount
+  }
+  type Acount {
+    name: String,
+    age: Int,
+    locatioin: String
   }
 `);
 
-var root = { hello: () => 'Hello world!' };
+var root = {
+  hello: () => 'Hello world!',
+  name: () => 'kobe',
+  age: () => 24,
+  habits: () => ['ball', 'ball', 'ball'],
+  getAcount() {
+    return {
+      name: 'curry',
+      age: 30,
+      location: 'golden state',
+    };
+  },
+};
 
 var app = express();
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true, // 开启调试器
-}));
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true, // 开启调试器
+  })
+);
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
