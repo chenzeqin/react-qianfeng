fe2f73a85c6a63b6e1e34be815937f5e7278ebb6
chenzeqin <sailor_chenzeqin@163.com>
2022-09-22 00:53:44 +0800

    dva: 配置dev server代理

--- dva-project/src/routes/Center/index.js
+++ dva-project/src/routes/Center/index.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import request from '../../utils/request'
 
 export default function Center() {
+  // 测试代理
+  useEffect(()=>{
+    request('/api/mmdb/movie/v3/list/hot.json?ct=%E6%B7%B1%E5%9C%B3&ci=30&channelId=4').then(res=>{
+      console.log(res)
+    })
+  },[])
   return (
     <div>Center</div>
   )
