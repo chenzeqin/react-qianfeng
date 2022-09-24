030dc24162b786b72339e6982e9560d41d79fe95
chenzeqin <sailor_chenzeqin@163.com>
2022-09-24 15:49:00 +0800

    umi: 404路由

--- react-ts/src/router-pages/center/index.tsx
+++ react-ts/src/router-pages/center/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Center(props:any) {
+export default function Center(props: any) {
   console.log('center props', props)
   return <div>Center</div>;
 }
--- umi-demo/.umirc.ts
+++ umi-demo/.umirc.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from 'umi';
 
+/*
+  配置式： 如果配置了该文件，路由模式切换为配置式
+  约定式：如果不配置，将根据pages页面编译，自动生成路由配置
+ */
+
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
   // path大小写不敏感
   routes: [
-    { path: '/', component: '@/pages/App' },
+    { exact: true, path: '/', component: '@/pages/App' },
     { path: '/films', component: '@/pages/films/index' },
     { path: '/cinemas', component: '@/pages/cinemas/index' },
+    { component: '@/pages/404' },
   ],
   fastRefresh: {},
 });
