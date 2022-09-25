d1d94832e409041b84d3a35467082e1de6025615
chenzeqin <sailor_chenzeqin@163.com>
2022-09-25 15:41:51 +0800

    umi: login组件修改 rcc->rfc

--- umi-demo/src/pages/Login/index.tsx
+++ umi-demo/src/pages/Login/index.tsx
@@ -1,41 +1,38 @@
-import React, { Component } from "react";
-import { IRouteComponentProps } from "umi";
+import React from "react";
+import { IRouteComponentProps, useHistory } from "umi";
 
 
 
-export default class Login extends Component<IRouteComponentProps> {
+export default function Login() {
+  const history = useHistory()
 
-  render() {
-    console.log(this.props)
-    return (
-      <div>
-        <h3>请登录！</h3>
+  return (
+    <div>
+      <h3>请登录！</h3>
+      <button
+        onClick={() => {
+          fetch("/user/login", {
+            method: "POST",
+            body: JSON.stringify({
+              name: "koke",
+              password: 123,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          })
+            .then(res => res.json())
+            .then((res) => {
+              console.log(res);
+              localStorage.setItem("token", res.token);
 
-        <button
-          onClick={() => {
-            fetch("/user/login", {
-              method: "POST",
-              body: JSON.stringify({
-                name: "koke",
-                password: 123,
-              }),
-              headers: {
-                "Content-Type": "application/json",
-              },
+              history.push("/center");
             })
-              .then(res => res.json())
-              .then((res) => {
-                console.log(res);
-                localStorage.setItem("token", res.token);
-
-                this.props.history.push("/center");
-              })
-              .catch((e) => console.error(e));
-          }}
-        >
-          一键登陆
-        </button>
-      </div>
-    );
-  }
+            .catch((e) => console.error(e));
+        }}
+      >
+        一键登陆
+      </button>
+    </div>
+  )
 }
