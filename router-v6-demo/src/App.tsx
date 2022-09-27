f92a9a06e303912ed9adec9a3411c55a8629cb8b
chenzeqin <sailor_chenzeqin@163.com>
2022-09-28 01:46:29 +0800

    router-v6: hello router v6

--- router-v6-demo/src/App.tsx
+++ router-v6-demo/src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Films from './views/Films';
+import Cinemas from './views/Cinemas';
+import Center from './views/Center';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter>
+        {/* 使用Routes替代v5 Switch */}
+        <Routes>
+          {/* 使用element属性替代v5 component */}
+          <Route path='/films' element={<Films></Films>}></Route>
+          <Route path='/cinemas' element={<Cinemas></Cinemas>}></Route>
+          <Route path='/center' element={<Center></Center>}></Route>
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
---
+++ router-v6-demo/src/views/Center/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Center() {
+  return (
+    <div>Center</div>
+  )
+}
