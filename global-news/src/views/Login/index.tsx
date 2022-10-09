b6cc813ba966db164573641895703b1d1ca761cf
chenzeqin <sailor_chenzeqin@163.com>
2022-10-10 01:13:43 +0800

    global-news:  登陆页面静态页面

--- global-news/src/views/Login/index.tsx
+++ global-news/src/views/Login/index.tsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import { Button, Checkbox, Form, Input } from 'antd';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import React from 'react';
+import styles from './index.module.scss'
+import ParticlesBackground from './ParticlesBackground'
 
 export default function Login() {
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
   return (
-    <div>Login</div>
+    <div className={styles.loginPage}>
+      <ParticlesBackground></ParticlesBackground>
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <h1>全球新闻发布管理系统</h1>
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: 'Please input your Username!' }]}
+        >
+          <Input prefix={<UserOutlined className="site-form-item-icon" />}
+            autoComplete="new-password" placeholder="Username" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: 'Please input your Password!' }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+            autoComplete="new-password"
+          />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className={styles.loginBtn}>
+            登 录
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   )
 }
