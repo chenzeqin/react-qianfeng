975d332871fd8fc5472391fea268bfd9da1e7ca3
chenzeqin <sailor_chenzeqin@163.com>
2022-10-10 01:35:24 +0800

    global-news:  particles暂时使用basic preset

--- global-news/src/views/Login/ParticlesBackground.tsx
+++ global-news/src/views/Login/ParticlesBackground.tsx
@@ -22,76 +22,78 @@ const ParticlesBackground = () => {
       init={particlesInit}
       loaded={particlesLoaded}
       options={{
-        background: {
-          color: {
-            value: "#0d47a1",
-          },
-        },
-        fpsLimit: 120,
-        interactivity: {
-          events: {
-            onClick: {
-              enable: true,
-              mode: "push",
-            },
-            onHover: {
-              enable: true,
-              mode: "repulse",
-            },
-            resize: true,
-          },
-          modes: {
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: "#ffffff",
-          },
-          links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outModes: {
-              default: "bounce",
-            },
-            random: false,
-            speed: 6,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              area: 800,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            value: { min: 1, max: 5 },
-          },
-        },
-        detectRetina: true,
+        preset: 'basic',
+        //  TODO: 示例的配置，粒子会越来越快，性能损耗，而官网demo配置导出报错，暂时使用基础preset。
+        // background: {
+        //   color: {
+        //     value: "#0d47a1",
+        //   },
+        // },
+        // fpsLimit: 120,
+        // interactivity: {
+        //   events: {
+        //     onClick: {
+        //       enable: true,
+        //       mode: "push",
+        //     },
+        //     onHover: {
+        //       enable: true,
+        //       mode: "repulse",
+        //     },
+        //     resize: true,
+        //   },
+        //   modes: {
+        //     push: {
+        //       quantity: 4,
+        //     },
+        //     repulse: {
+        //       distance: 200,
+        //       duration: 0.4,
+        //     },
+        //   },
+        // },
+        // particles: {
+        //   color: {
+        //     value: "#ffffff",
+        //   },
+        //   links: {
+        //     color: "#ffffff",
+        //     distance: 150,
+        //     enable: true,
+        //     opacity: 0.5,
+        //     width: 1,
+        //   },
+        //   collisions: {
+        //     enable: true,
+        //   },
+        //   move: {
+        //     direction: "none",
+        //     enable: true,
+        //     outModes: {
+        //       default: "bounce",
+        //     },
+        //     random: false,
+        //     speed: 6,
+        //     straight: false,
+        //   },
+        //   number: {
+        //     density: {
+        //       enable: true,
+        //       area: 800,
+        //     },
+        //     value: 80,
+        //   },
+        //   opacity: {
+        //     value: 0.5,
+        //   },
+        //   shape: {
+        //     type: "circle",
+        //   },
+        //   size: {
+        //     value: { min: 1, max: 5 },
+        //   },
+        // },
+        // detectRetina: true,
       }}
     />
   );
